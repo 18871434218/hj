a4128bfa8ba820ef47fda435c536121cff9f2f75
wangdong <18871434218@163.com>
2020-07-17 06:41:34 +0800

    2323

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Home">
+    <div style="text-align:center;font-size:24px; padding-top:40px"><h1>欢迎{{userName}}接入舜宇车联网</h1></div>
+  
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      userName: "",
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Login">
+    <div style="text-align:center;font-size:24px; padding-top:40px"><h1>{{ msg }}</h1></div>
+    <div style="text-align:center;font-size:20px"><h1>{{ msg1 }}</h1></div>
+  
+    <button>你是</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: '舜宇车联网平台',
+      msg1:"车端模拟系统"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
